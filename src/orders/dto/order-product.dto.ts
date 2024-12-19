4be603983e5bcf4458ba87e7757791bd758643ca
shoroukshorouk <mahmoudwardany18@gmail.com>
2024-12-20 01:09:12 +0200

    chore(order) refacto order

--- src/orders/dto/order-product.dto.ts
+++ src/orders/dto/order-product.dto.ts
@@ -1,10 +1,10 @@
-/* eslint-disable prettier/prettier */
-import { IsNumber, IsPositive } from "class-validator";
-export class OrderProductDto{
-    @IsNumber({maxDecimalPlaces:2})
-    @IsPositive()
-    order_quantity:number
-    @IsNumber()
-    @IsPositive()
-    productId:number
+import { IsNumber, IsPositive } from 'class-validator';
+export class OrderProductDto {
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  order_quantity: number;
+
+  @IsNumber()
+  @IsPositive()
+  productId: number;
 }
--- src/orders/services/orders.service.ts
+++ src/orders/services/orders.service.ts
@@ -8,7 +8,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { EntityManager, Repository } from 'typeorm';
 import { CreateOrderDto } from '../dto/create-order.dto';
 import { OrderEntity } from '../entities/order.entity';
-import { OrderProductsEntity } from '../entities/order-product.entity';
 import { UserEntity } from 'src/users/entities/user.entity';
 import { ProductEntity } from 'src/products/entities/product.entity';
 import { AuditService } from 'src/audit/audit.services';
@@ -24,41 +23,25 @@ export class OrdersService {
     @InjectRepository(OrderEntity)
     private readonly orderRepository: Repository<OrderEntity>,
     @InjectRepository(ProductEntity)
-    private readonly productRepository: Repository<ProductEntity>,
     private readonly entityManager: EntityManager,
     private readonly auditService: AuditService,
     private readonly productServices: ProductsService,
   ) {}
 
   async createOrder(createOrderDto: CreateOrderDto, user: UserEntity) {
-    const shippingEntity = new ShippingEntity();
-    shippingEntity.phone = createOrderDto.shippingAddress.phone;
-    shippingEntity.name = createOrderDto.shippingAddress.name;
-    shippingEntity.address = createOrderDto.shippingAddress.address;
-    shippingEntity.city = createOrderDto.shippingAddress.city;
-
+    const shippingEntity = await this.createShippingEntity(
+      createOrderDto.shippingAddress,
+    );
     const order = new OrderEntity();
     order.user = user;
     order.shippingAddress = shippingEntity;
     order.shippedAt = null;
     order.products = [];
 
-    const orderProducts: OrderProductDto[] = createOrderDto.orderProducts;
-    let totalOrderPrice = 0;
+    const totalOrderPrice = this.calculateTotalPrice(
+      createOrderDto.orderProducts,
+    );
 
-    for (const productDto of orderProducts) {
-      const product = await this.findProductById(productDto.productId);
-
-      const orderProduct = new OrderProductsEntity();
-      orderProduct.order_quantity = productDto.order_quantity;
-      orderProduct.product = product;
-      const productPrice = product.price;
-      const priceTotal = productPrice * orderProduct.order_quantity;
-      totalOrderPrice += priceTotal;
-      order.products.push(orderProduct);
-    }
-
-    order.total_price = totalOrderPrice;
     try {
       await this.entityManager.transaction(
         async (transactionalEntityManager) => {
@@ -66,26 +49,17 @@ export class OrdersService {
           await transactionalEntityManager.save(OrderEntity, order);
         },
       );
+
       this.auditService.logOrderPlacement(user.id, order.id);
       const savedOrder = await this.findOne(order.id);
-
-      return { savedOrder, totalPrice: order.total_price };
+      return { savedOrder, totalPrice: totalOrderPrice };
     } catch (error) {
       this.auditService.failedOrder(user.id, order.id, error, error.stack);
-
       throw new InternalServerErrorException(
         'Failed to create the order',
         error.message,
       );
     }
-  }
-
-  async findProductById(id: number) {
-    const product = this.productServices.findOne(id);
-    if (!product) {
-      throw new NotFoundException(`Product with ID ${id} not found.`);
-    }
-    return product;
   }
 
   async findAll(): Promise<OrderEntity[]> {
@@ -94,7 +68,7 @@ export class OrdersService {
     });
   }
 
-  async findOne(id: number): Promise<OrderEntity | undefined> {
+  async findOne(id: number): Promise<OrderEntity> {
     return this.orderRepository
       .createQueryBuilder('order')
       .leftJoinAndSelect('order.shippingAddress', 'shippingAddress')
@@ -106,73 +80,42 @@ export class OrdersService {
       .where('order.id = :id', { id })
       .getOne();
   }
+
   async updateOrderStatus(
     id: number,
     updateOrderDto: UpdateOrderStatus,
     user: UserEntity,
   ) {
-    try {
-      const order = await this.findOne(id);
+    const order = await this.findOne(id);
 
-      if (!order) {
-        throw new NotFoundException(`Order with ID ${id} not found.`);
-      }
-
-      if (
-        order.status === OrderStatus.DELIVERED ||
-        order.status === OrderStatus.CANCELLED
-      ) {
-        throw new BadRequestException(`Order is already ${order.status}`);
-      }
-
-      if (
-        order.status === OrderStatus.PROCESSING &&
-        updateOrderDto.status !== OrderStatus.SHIPPED
-      ) {
-        throw new BadRequestException(
-          `Cannot mark as Shipped before Processing.`,
-        );
-      }
-
-      if (updateOrderDto.status === OrderStatus.SHIPPED) {
-        order.shippedAt = new Date();
-      }
-
-      if (updateOrderDto.status === OrderStatus.DELIVERED) {
-        order.orderAt = new Date();
-      }
-
-      order.status = updateOrderDto.status;
-      order.updatedBy = user;
-
-      await this.orderRepository.save(order);
-
-      if (updateOrderDto.status === OrderStatus.DELIVERED) {
-        await this.updateProductStock(order, OrderStatus.DELIVERED);
-      }
-
-      return order;
-    } catch (error) {
-      throw new InternalServerErrorException(
-        'Failed to update order status',
-        error.message,
-      );
+    if (!order) {
+      throw new NotFoundException(`Order with ID ${id} not found.`);
     }
+
+    this.validateOrderStatusUpdate(order, updateOrderDto);
+
+    order.status = updateOrderDto.status;
+    order.updatedBy = user;
+
+    if (updateOrderDto.status === OrderStatus.SHIPPED) {
+      order.shippedAt = new Date();
+    }
+
+    if (updateOrderDto.status === OrderStatus.DELIVERED) {
+      order.orderAt = new Date();
+      await this.updateProductStock(order, OrderStatus.DELIVERED);
+    }
+
+    await this.orderRepository.save(order);
+    return order;
   }
 
-  async updateProductStock(order: OrderEntity, status: OrderStatus) {
-    try {
-      for (const op of order.products) {
-        await this.productServices.updateStock(
-          op.product.id,
-          op.order_quantity,
-          status,
-        );
-      }
-    } catch (error) {
-      throw new InternalServerErrorException(
-        'Failed to update product stock',
-        error.message,
+  private async updateProductStock(order: OrderEntity, status: OrderStatus) {
+    for (const op of order.products) {
+      await this.productServices.updateStock(
+        op.product.id,
+        op.order_quantity,
+        status,
       );
     }
   }
@@ -189,14 +132,69 @@ export class OrdersService {
 
     order.updatedBy = user;
     order.status = OrderStatus.CANCELLED;
-
     await this.orderRepository.save(order);
     await this.updateProductStock(order, OrderStatus.CANCELLED);
 
     return order;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  //helper
+  async findAllProductById(id: number) {
+    const product = await this.productServices.findOne(id);
+    if (!product) {
+      throw new NotFoundException(`Product Not Found with id : ${id}`);
+    }
+    return product;
+  }
+
+  private async calculateTotalPrice(
+    orderProducts: OrderProductDto[],
+  ): Promise<number> {
+    let total = 0;
+
+    for (const productDto of orderProducts) {
+      const product = await this.productServices.findOne(productDto.productId);
+      total += productDto.order_quantity * product.price;
+    }
+
+    return total;
+  }
+
+  private validateOrderStatusUpdate(
+    order: OrderEntity,
+    updateOrderDto: UpdateOrderStatus,
+  ) {
+    if (
+      order.status === OrderStatus.DELIVERED ||
+      order.status === OrderStatus.CANCELLED
+    ) {
+      throw new BadRequestException(`Order is already ${order.status}`);
+    }
+
+    if (
+      order.status === OrderStatus.PROCESSING &&
+      updateOrderDto.status !== OrderStatus.SHIPPED
+    ) {
+      throw new BadRequestException(
+        `Cannot mark as Shipped before Processing.`,
+      );
+    }
+  }
+
+  async findProductById(id: number) {
+    const product = this.productServices.findOne(id);
+    if (!product) {
+      throw new NotFoundException(`Product with ID ${id} not found.`);
+    }
+    return product;
+  }
+
+  private async createShippingEntity(shippingAddress) {
+    const shippingEntity = new ShippingEntity();
+    shippingEntity.phone = shippingAddress.phone;
+    shippingEntity.name = shippingAddress.name;
+    shippingEntity.address = shippingAddress.address;
+    shippingEntity.city = shippingAddress.city;
+    return shippingEntity;
   }
 }
